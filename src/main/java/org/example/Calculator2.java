c97b065fafe1dfbb0f2196b3f46cd10d15ad583c
jaxpro-code <jax.bradley.pro@gmail.com>
2026-04-10 12:07:13 -0400

    finished cal 2

--- src/main/java/org/example/Calculator2.java
+++ src/main/java/org/example/Calculator2.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator2 {
     public static void main(String[] args) {
-    // the deposit, inter
+    // the deposit, interest rate, number of years
+    // fv = p *(1+(r/365))^(365*t)
+    // fv- p
+    //output the furture vaule and total interest
+        Scanner info = new Scanner(System.in);
+        System.out.println("what is your deposit?");
+        double P = info.nextDouble();
+
+        System.out.println("what is your interest rate?");
+        double rate = info.nextDouble();
+        double r = (rate/100);
+
+        System.out.println("How many years is your investment?");
+        double t = info.nextDouble();
+
+        double fv = P * Math.pow((1+(r/365)),365*t);
+
+
+
 
 }
 }
